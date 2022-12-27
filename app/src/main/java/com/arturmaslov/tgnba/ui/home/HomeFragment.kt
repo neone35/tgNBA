160a7a9c7feb344191b09657959ef224f2efb464
Artur Maslov <aartasas@gmail.com>
2022-12-27 19:11:15 +0200

    initial commit with dependencies and styles added

---
+++ app/src/main/java/com/arturmaslov/tgnba/ui/home/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.arturmaslov.tgnba.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.arturmaslov.tgnba.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeVM =
+            ViewModelProvider(this).get(HomeVM::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeVM.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/arturmaslov/tgnba/ui/player/PlayerFragment.kt
@@ -0,0 +1,40 @@
+package com.arturmaslov.tgnba.ui.player
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.arturmaslov.tgnba.databinding.FragmentPlayerBinding
+
+class PlayerFragment : Fragment() {
+
+    private var _binding: FragmentPlayerBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val playerVM = ViewModelProvider(this).get(PlayerVM::class.java)
+        _binding = FragmentPlayerBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        playerVM.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
